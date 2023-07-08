8ddd8f140587025ca591c71915f29242e4991b2a
Bwhitman33 <bwhitman33@gmail.com>
2023-07-08 14:57:40 -0400

    fixed edits, comments, deletes. Almost done.

--- public/js/edit-post.js
+++ public/js/edit-post.js
@@ -1,9 +1,9 @@
 async function editFormHandler(event) {
     event.preventDefault();
 
-    const title = document.querySelector('input[name="post-title"]').ariaValueMax.trim();
+    const title = document.querySelector('input[name="post-title"]').value.trim();
     const id = window.location.toString().split('/')[
-        window.locationtoString().split('/').length - 1
+        window.location.toString().split('/').length - 1
 ];
 const response = await fetch(`/api/posts/${id}`, {
     method: 'PUT',
